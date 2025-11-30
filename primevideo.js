c1bf2251fa099e5c6479778f6aadd67ef5df4e81
Sean O'Connor <soc1641@icloud.com>
2025-11-30 17:28:43 -0600

    allowed you to toggle prime video autoplay and also added the permissions to manifest

--- primevideo.js
+++ primevideo.js
@@ -1,10 +1,20 @@
-function disableAutoplay(){
-    const hideBtn = document.querySelector(".atvwebplayersdk-nextupcardhide-button");
-
-     if (hideBtn) {
-        hideBtn.click();
-        console.log("Clicked 'Hide' button to stop autoplay");
+if (window.primeVideoAutoplayDisabled) {
+    // Stop the interval
+    clearInterval(window.primeVideoInterval);
+    window.primeVideoAutoplayDisabled = false;
+    console.log("Prime Video autoplay disabler STOPPED");
+} else {
+    // Start the interval
+    function disableAutoplay() {
+        const hideBtn = document.querySelector(".atvwebplayersdk-nextupcardhide-button");
+        
+        if (hideBtn) {
+            hideBtn.click();
+            console.log("Clicked 'Hide' button to stop autoplay");
+        }
     }
+    
+    window.primeVideoInterval = setInterval(disableAutoplay, 1000);
+    window.primeVideoAutoplayDisabled = true;
+    console.log("Prime Video autoplay disabler STARTED");
 }
-
-setInterval(disableAutoplay, 1000);
